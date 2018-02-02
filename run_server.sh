4a77d192947e334c4533e89fb3db267d42f176ce
Luis Remis <luis.remis@intel.com>
2018-02-01 19:59:13 -0800

    Update scripts for logging cerr

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,9 @@
 scons -j123
-rm log.log screen.log dump.log
+rm log.log
 rm -r db
 mkdir db
 mkdir db/native_format
 mkdir db/native_format/pngs
 mkdir db/native_format/jpgs
 mkdir db/native_format/descriptors
-./vdms
+./vdms 2> log.log
--- tests/python/run_python_tests.sh
+++ tests/python/run_python_tests.sh
@@ -7,7 +7,7 @@ mkdir db/images/pngs
 mkdir db/images/jpgs
 mkdir db/descriptors
 
-../../vdms -cfg config-tests.json > screen.log &
+../../vdms -cfg config-tests.json > screen.log 2> log.log &
 python main.py -v
 
 sleep 1
