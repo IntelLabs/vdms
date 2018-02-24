bfb5f0dd09d0af8a74cad6d9578ae26476ab605a
Luis Remis <luis.remis@intel.com>
2018-02-23 17:01:23 -0800

    Fix scripts to run server and tests

--- tests/python/run_python_tests.sh
+++ tests/python/run_python_tests.sh
@@ -1,16 +1,7 @@
-rm log.log screen.log dump.log
-rm -r db
-mkdir db
-mkdir db/test-graph
-mkdir db/images
-mkdir db/images/pngs
-mkdir db/images/jpgs
-mkdir db/descriptors
+sh clean.sh
 
 ../../vdms -cfg config-tests.json > screen.log 2> log.log &
 python main.py -v
 
 sleep 1
 pkill vdms
-
-dumpg db/test-graph > dump.log
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
-cd .. && scons -j123 && cd tests
 sh cleandbs.sh
+
 ./query_tests
 echo 'Running python tests...'
 cd python
