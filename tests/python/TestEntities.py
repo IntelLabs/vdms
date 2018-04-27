f54da2cbddd8db905ca96b5c0bc1393ff5882929
Luis Remis <luis.remis@intel.com>
2018-04-27 09:58:31 -0700

    Update and Add testing

---
+++ tests/python/TestEntities.py
@@ -0,0 +1,172 @@
+#
+# The MIT License
+#
+# @copyright Copyright (c) 2017 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from threading import Thread
+import sys
+import os
+import urllib
+import time
+import json
+import unittest
+import numpy as np
+import vdms
+
+hostname = "localhost"
+port = 55557
+
+class TestEntities(unittest.TestCase):
+
+    def addEntity(self, thID=0):
+
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        props = {}
+        props["name"] = "Luis"
+        props["lastname"] = "Ferro"
+        props["age"] = 27
+        props["threadid"] = thID
+
+        addEntity = {}
+        addEntity["properties"] = props
+        addEntity["class"] = "AwesomePeople"
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        response, res_arr = db.query(all_queries)
+        response = json.loads(response)
+
+        self.assertEqual(response[0]["AddEntity"]["status"], 0)
+
+    def findEntity(self, thID):
+
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        constraints = {}
+        constraints["threadid"] = ["==",thID]
+
+        findEntity = {}
+        findEntity["constraints"] = constraints
+        findEntity["class"] = "AwesomePeople"
+
+        results = {}
+        results["list"] = ["name", "lastname", "threadid"]
+        findEntity["results"] = results
+
+        query = {}
+        query["FindEntity"] = findEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        response, res_arr = db.query(all_queries)
+        response = json.loads(response)
+        # print vdms.aux_print_json(response)
+
+        self.assertEqual(response[0]["FindEntity"]["status"], 0)
+        self.assertEqual(response[0]["FindEntity"]["entities"][0]
+                                    ["lastname"], "Ferro")
+        self.assertEqual(response[0]["FindEntity"]["entities"][0]
+                                    ["threadid"], thID)
+
+    def test_runMultipleAdds(self):
+
+        simultaneous = 1000;
+        thread_arr = []
+        for i in range(1,simultaneous):
+            thread_add = Thread(target=self.addEntity,args=(i,) )
+            thread_add.start()
+            thread_arr.append(thread_add)
+
+        for i in range(1,simultaneous):
+            thread_find = Thread(target=self.findEntity,args=(i,) )
+            thread_find.start()
+            thread_arr.append(thread_find)
+
+        for th in thread_arr:
+            th.join();
+
+    def test_addFindEntity(self):
+        self.addEntity(9000);
+        self.findEntity(9000);
+
+    def test_addEntityWithLink(self):
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        all_queries = []
+
+        props = {}
+        props["name"] = "Luis"
+        props["lastname"] = "Ferro"
+        props["age"] = 27
+
+        addEntity = {}
+        addEntity["_ref"] = 32
+        addEntity["properties"] = props
+        addEntity["class"] = "AwesomePeople"
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries.append(query)
+
+        props = {}
+        props["name"] = "Luis"
+        props["lastname"] = "Bueno"
+        props["age"] = 27
+
+        link = {}
+        link["ref"] = 32
+        link["direction"] = "in"
+        link["class"] = "Friends"
+
+        addEntity = {}
+        addEntity["properties"] = props
+        addEntity["class"] = "AwesomePeople"
+        addEntity["link"] = link
+
+        img_params = {}
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries.append(query)
+
+        # print json.dumps(all_queries)
+        # vdms.aux_print_json(all_queries)
+
+        response, res_arr = db.query(all_queries)
+        response = json.loads(response)
+        # vdms.aux_print_json(response)
+
+        self.assertEqual(response[0]["AddEntity"]["status"], 0)
+        self.assertEqual(response[1]["AddEntity"]["status"], 0)
---
+++ tests/python/TestEntitiesBlobs.py
@@ -0,0 +1,161 @@
+#
+# The MIT License
+#
+# @copyright Copyright (c) 2017 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from threading import Thread
+import sys
+import os
+import urllib
+import time
+import json
+import unittest
+import numpy as np
+import vdms
+
+hostname = "localhost"
+port = 55557
+
+class TestEntitiesBlob(unittest.TestCase):
+
+    def test_addEntityWithBlob(self, thID=0):
+
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        props = {}
+        props["name"] = "Luis"
+        props["lastname"] = "Ferro"
+        props["age"] = 27
+        props["threadid"] = thID
+
+        addEntity = {}
+        addEntity["properties"] = props
+        addEntity["class"] = "AwesomePeople"
+        addEntity["blob"] = True
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        blob_arr = []
+        fd = open("../test_images/brain.png")
+        blob_arr.append(fd.read())
+
+        response, res_arr = db.query(all_queries, [blob_arr])
+        response = json.loads(response)
+
+        self.assertEqual(response[0]["AddEntity"]["status"], 0)
+
+    def test_addEntityWithBlobNoBlob(self, thID=0):
+
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        props = {}
+        props["name"] = "Luis"
+        props["lastname"] = "Ferro"
+        props["age"] = 27
+        props["threadid"] = thID
+
+        addEntity = {}
+        addEntity["properties"] = props
+        addEntity["class"] = "AwesomePeople"
+        addEntity["blob"] = True
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        response, res_arr = db.query(all_queries)
+        response = json.loads(response)
+        # vdms.aux_print_json(response)
+
+        self.assertEqual(response[0]["status"], -1)
+        self.assertEqual(response[0]["info"],
+                         "Expected blobs: 1. Received blobs: 0")
+
+    def test_addEntityWithBlobAndFind(self, thID=0):
+
+        db = vdms.VDMS()
+        db.connect(hostname, port)
+
+        props = {}
+        props["name"] = "Tom"
+        props["lastname"] = "Slash"
+        props["age"] = 27
+        props["id"] = 45334
+
+        addEntity = {}
+        addEntity["properties"] = props
+        addEntity["class"] = "NotSoAwesome"
+        addEntity["blob"] = True
+
+        query = {}
+        query["AddEntity"] = addEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        blob_arr = []
+        fd = open("../test_images/brain.png")
+        blob_arr.append(fd.read())
+
+        response, res_arr = db.query(all_queries, [blob_arr])
+        response = json.loads(response)
+
+        self.assertEqual(response[0]["AddEntity"]["status"], 0)
+
+        constraints = {}
+        constraints["id"] = ["==", 45334]
+
+        results = {}
+        results["blob"] = True
+        results["list"] = ["name"]
+
+        FindEntity = {}
+        FindEntity["constraints"] = constraints
+        FindEntity["class"] = "NotSoAwesome"
+        FindEntity["results"] = results
+
+        query = {}
+        query["FindEntity"] = FindEntity
+
+        all_queries = []
+        all_queries.append(query)
+
+        blob_arr = []
+        fd = open("../test_images/brain.png")
+        blob_arr.append(fd.read())
+
+        response, res_arr = db.query(all_queries)
+
+        self.assertEqual(len(res_arr), len(blob_arr))
+        self.assertEqual(len(res_arr[0]), len(blob_arr[0]))
+        self.assertEqual((res_arr[0]), (blob_arr[0]))
+
