9523084543346cc27fa96a776fab1e746c922c09
Luis Remis <luis.remis@intel.com>
2019-03-29 10:26:25 -0700

    Add/Reorg Descriptors Tests

--- tests/python/TestEngineDescriptors.py
+++ tests/python/TestEngineDescriptors.py
@@ -51,151 +51,35 @@ class TestDescriptors(TestCommand.TestCommand):
         # Check success
         self.assertEqual(response[0]["AddDescriptorSet"]["status"], 0)
 
-    def test_addSet(self):
+    def test_addDifferentSets(self):
 
-        db = self.create_connection()
+        self.addSet("128-L2-FaissFlat", 128, "L2", "FaissFlat")
+        self.addSet("128-IP-FaissFlat", 128, "IP", "FaissFlat")
+        self.addSet("128-L2-FaissIVFFlat", 128, "L2", "FaissIVFFlat")
+        self.addSet("128-IP-FaissIVFFlat", 128, "IP", "FaissIVFFlat")
+        self.addSet("128-L2-TileDBDense", 128, "L2", "TileDBDense")
+        self.addSet("128-L2-TileDBSparse", 128, "L2", "TileDBSparse")
 
-        all_queries = []
+        self.addSet("4075-L2-FaissFlat", 4075, "L2", "FaissFlat")
+        self.addSet("4075-IP-FaissFlat", 4075, "IP", "FaissFlat")
+        self.addSet("4075-L2-FaissIVFFlat", 4075, "L2", "FaissIVFFlat")
+        self.addSet("4075-IP-FaissIVFFlat", 4075, "IP", "FaissIVFFlat")
+        self.addSet("4075-L2-TileDBDense", 4075, "L2", "TileDBDense")
 
-        descriptor_set = {}
-        descriptor_set["name"] = "features_xd"
-        descriptor_set["dimensions"] = 1024*4
-
-        query = {}
-        query["AddDescriptorSet"] = descriptor_set
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries)
-
-        # Check success
-        self.assertEqual(response[0]["AddDescriptorSet"]["status"], 0)
-
-    def test_addSetAndDescriptors(self):
-
-        db = self.create_connection()
-
-        all_queries = []
-
-        # Add Set
-        set_name = "features_128d"
-        dims = 1024
-        descriptor_set = {}
-        descriptor_set["name"] = set_name
-        descriptor_set["dimensions"] = dims
-
-        query = {}
-        query["AddDescriptorSet"] = descriptor_set
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries)
-        self.assertEqual(response[0]["AddDescriptorSet"]["status"], 0)
-
-        # Add Descriptors
-        all_queries = []
-        descriptor_blob = []
-
-        x = np.zeros(dims)
-        x = x.astype('float32')
-        # print type(x[0])
-        # print "size: ", len(x.tobytes())/4
-        descriptor_blob.append(x.tobytes())
-
-        descriptor = {}
-        descriptor["set"] = set_name
-
-        query = {}
-        query["AddDescriptor"] = descriptor
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries, [descriptor_blob])
-
-        # Check success
-        self.assertEqual(response[0]["AddDescriptor"]["status"], 0)
-
-    def test_addSetAndDescriptorsDimMismatch(self):
+    def test_addDescriptorsx1000FaissIVFFlat(self):
 
         db = self.create_connection()
 
         all_queries = []
 
         # Add Set
-        set_name = "features_64d_dim_mismatched"
-        dims = 64
-        descriptor_set = {}
-        descriptor_set["name"] = set_name
-        descriptor_set["dimensions"] = dims
-
-        query = {}
-        query["AddDescriptorSet"] = descriptor_set
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries)
-        self.assertEqual(response[0]["AddDescriptorSet"]["status"], 0)
-
-        # Add Descriptors
-        all_queries = []
-        descriptor_blob = []
-
-        x = np.zeros(dims/2)
-        x = x.astype('float32')
-        # print type(x[0])
-        # print "size: ", len(x.tobytes())/4
-        descriptor_blob.append(x.tobytes())
-
-        descriptor = {}
-        descriptor["set"] = set_name
-
-        query = {}
-        query["AddDescriptor"] = descriptor
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries, [descriptor_blob])
-
-        # Check success
-        self.assertEqual(response[0]["status"], -1)
-        self.assertEqual(response[0]["info"], "Blob Dimensions Mismatch")
-
-        # Add Descriptors
-        all_queries = []
-        descriptor_blob = []
-
-        x = np.zeros(dims)[:-1]
-        x = x.astype('float32')
-        # print type(x[0])
-        # print "size: ", len(x.tobytes())/4
-        descriptor_blob.append(x.tobytes())
-
-        descriptor = {}
-        descriptor["set"] = set_name
-
-        query = {}
-        query["AddDescriptor"] = descriptor
-
-        all_queries.append(query)
-
-        response, img_array = db.query(all_queries, [descriptor_blob])
-
-        # Check success
-        self.assertEqual(response[0]["status"], -1)
-        self.assertEqual(response[0]["info"], "Blob Dimensions Mismatch")
-
-    def test_addDescriptorsx1000(self):
-
-        db = self.create_connection()
-
-        all_queries = []
-
-        # Add Set
-        set_name = "features_128dx1000"
+        set_name = "faissivfflat_ip_128dx1000"
         dims = 128
         descriptor_set = {}
         descriptor_set["name"] = set_name
         descriptor_set["dimensions"] = dims
+        descriptor_set["metric"] = "IP"
+        descriptor_set["engine"] = "FaissIVFFlat"
 
         query = {}
         query["AddDescriptorSet"] = descriptor_set
@@ -231,18 +115,21 @@ class TestDescriptors(TestCommand.TestCommand):
         for x in range(0,total-1):
             self.assertEqual(response[x]["AddDescriptor"]["status"], 0)
 
-    def test_classifyDescriptor(self):
+
+    def test_addDescriptorsx1000TileDBSparse(self):
 
         db = self.create_connection()
 
         all_queries = []
 
         # Add Set
-        set_name = "features_128d_4_classify"
+        set_name = "tiledbsparse_l2_128dx1000"
         dims = 128
         descriptor_set = {}
         descriptor_set["name"] = set_name
         descriptor_set["dimensions"] = dims
+        descriptor_set["metric"] = "L2"
+        descriptor_set["engine"] = "TileDBSparse"
 
         query = {}
         query["AddDescriptorSet"] = descriptor_set
@@ -257,11 +144,7 @@ class TestDescriptors(TestCommand.TestCommand):
 
         total = 1000;
 
-        class_counter = -1
-        for i in range(0,total-1):
-            if ((i % 4) == 0):
-                class_counter += 1
-
+        for i in range(1,total):
             x = np.ones(dims)
             x[2] = 2.34 + i*20
             x = x.astype('float32')
@@ -269,7 +152,7 @@ class TestDescriptors(TestCommand.TestCommand):
 
             descriptor = {}
             descriptor["set"] = set_name
-            descriptor["label"] = "class" + str(class_counter)
+            descriptor["label"] = "classX"
 
             query = {}
             query["AddDescriptor"] = descriptor
@@ -282,27 +165,52 @@ class TestDescriptors(TestCommand.TestCommand):
         for x in range(0,total-1):
             self.assertEqual(response[x]["AddDescriptor"]["status"], 0)
 
+    def test_addDescriptorsx1000TileDBDense(self):
 
-        descriptor = {}
-        descriptor["set"] = set_name
+        db = self.create_connection()
+
+        all_queries = []
+
+        # Add Set
+        set_name = "tiledbdense_l2_128dx1000"
+        dims = 128
+        descriptor_set = {}
+        descriptor_set["name"] = set_name
+        descriptor_set["dimensions"] = dims
+        descriptor_set["metric"] = "L2"
+        descriptor_set["engine"] = "TileDBDense"
 
         query = {}
-        query["ClassifyDescriptor"] = descriptor
+        query["AddDescriptorSet"] = descriptor_set
 
-        for i in range(2, total//10, 4):
-            all_queries = []
-            descriptor_blob = []
+        all_queries.append(query)
 
+        response, img_array = db.query(all_queries)
+        # print(json.dumps(all_queries, indent=4, sort_keys=False))
+        self.assertEqual(response[0]["AddDescriptorSet"]["status"], 0)
+
+        all_queries = []
+        descriptor_blob = []
+
+        total = 1000;
+
+        for i in range(1,total):
             x = np.ones(dims)
-            x[2] = 2.34 + i*20 # Calculated to be of class1
+            x[2] = 2.34 + i*20
             x = x.astype('float32')
             descriptor_blob.append(x.tobytes())
 
+            descriptor = {}
+            descriptor["set"] = set_name
+            descriptor["label"] = "classX"
+
+            query = {}
+            query["AddDescriptor"] = descriptor
+
             all_queries.append(query)
 
-            response, img_array = db.query(all_queries, [descriptor_blob])
+        response, img_array = db.query(all_queries, [descriptor_blob])
 
-            # Check success
-            self.assertEqual(response[0]["ClassifyDescriptor"]["status"], 0)
-            self.assertEqual(response[0]["ClassifyDescriptor"]
-                                      ["label"], "class" + str(int(i/4)))
+        # Check success
+        for x in range(0,total-1):
+            self.assertEqual(response[x]["AddDescriptor"]["status"], 0)
