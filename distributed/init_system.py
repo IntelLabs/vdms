ccc466ba79a807b8ee3c29ce6822ddec8fcac393
Dennis Matthews <dennis.matthews.87@gmail.com>
2020-09-11 18:04:32 -0700

    checking in version that does everything but reset the port within the docker container  for the config-vdms.json

--- distributed/init_system.py
+++ distributed/init_system.py
@@ -3,16 +3,51 @@ import json
 import pyaml
 import sys
 
+from collections import OrderedDict
+
 config_file_name = sys.argv[1]
 print(config_file_name)
 config_file = open(config_file_name, "r")
 config = json.load(config_file)
-controller = config["controller"]
+
+manager = config["manager"]
 workers = config["workers"]
 
-output_yaml_dict = {}
-output_yaml_dict["version"] = '3.8'
-services = []
-output_controller = {}
+output = OrderedDict()
+output["version"] = "\'2.0\'"
+services = {}
 
-print(pyaml.dump(workers))
+output_manager = {}
+output_manager["container_name"] = "\'" + manager["name"] + "\'"
+output_manager_ports = []
+output_manager_ports.append( "\'" + str(manager["port"]) + ":" + str(manager["port"]) + "\'")
+output_manager["ports"] = output_manager_ports
+output_manager_build = {}
+output_manager_build["context"] = "\'manager\'"
+output_manager_build["dockerfile"] = "\'Dockerfile\'"
+output_manager["build"] = output_manager_build
+output_manager_environment = {}
+output_manager_environment["NETWORK_PORT"] = manager["port"]
+output_manager["environment"] = output_manager_environment
+services[manager["name"]] = output_manager
+
+for this_worker in workers:
+    output_worker = {}
+    output_worker["container_name"] = "\'" + this_worker["name"] + "\'"
+    output_worker_ports = []
+    output_worker_ports.append( "\'" + str(this_worker["port"]) + ":" + str(this_worker["port"]) + "\'")
+    output_worker["ports"] = output_worker_ports
+    output_worker_build = {}
+    output_worker_build["context"] = "\'worker\'"
+    output_worker_build["dockerfile"] = "\'Dockerfile\'"
+    output_worker["build"] = output_worker_build
+    output_worker_environment = {}
+    output_worker_environment["NETWORK_PORT"] = this_worker["port"]
+    output_worker["environment"] = output_worker_environment
+    services[this_worker["name"]] = output_worker
+
+output["services"] = services
+print(pyaml.dump(output))
+
+output_file = open("docker_compose.yml", "w")
+pyaml.dump(output, output_file)
---
+++ distributed/worker/config_vdms.py
@@ -0,0 +1,8 @@
+import os
+import json
+#open the file
+
+#if key is present, get the port number from the environment
+os.environ["NETWORK_PORT"]
+
+#write the file back
