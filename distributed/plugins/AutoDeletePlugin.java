342c619971427092e387694ef33fc54b7b7a4a6a
Dennis Matthews <dennis.matthews.87@gmail.com>
2020-11-18 16:11:38 -0800

    checking in start to auto- queries (not fully functional yet) also fixed plugin CMakeLists.txt - left out the location of the jar needed to process json
    http://www.java2s.com/Code/JarDownload/json-simple/json-simple-1.1.1.jar.zip
    that needs to be moved to /usr/share/java

---
+++ distributed/plugins/AutoDeletePlugin.java
@@ -0,0 +1,91 @@
+import java.io.IOException;
+
+import java.lang.System;
+
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import java.nio.ByteBuffer;
+
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+
+public class AutoDeletePlugin extends Plugin
+{
+
+   public AutoDeletePlugin()
+   {
+      byte[] initSize = new byte[] {(byte)0xfc, (byte)0xff, (byte)0xff, (byte)0xff};
+      byte[] initPayload = ByteBuffer.allocate(4).putInt(0).array();
+      VdmsTransaction initSequence = new VdmsTransaction(initSize, initPayload);
+      
+      String sourceNodes = System.getenv("SOURCES");
+      String sourceNodesArray[] = sourceNodes.split(",");
+      int sourceNodeCount = sourceNodesArray.length;
+      String destinationNodes = System.getenv("DESTINATIONS");
+      String destinationNodesArray[] = destinationNodes.split(",");
+      int destinationNodeCount = destinationNodesArray.length;
+      
+      for(int i = 0; i < sourceNodeCount; i++)
+      {
+         String connectionInfo[] = sourceNodesArray[i].split(":");
+         int connectionPort = Integer.parseInt(connectionInfo[1]);
+         System.out.println(connectionInfo[0] + " " + Integer.toString(connectionPort));
+         try 
+         {
+            Socket thisSocket = new Socket(connectionInfo[0], connectionPort);
+            PublisherServiceThread thisServerServivceThread = new AutoPublisherServiceThread(this, thisSocket, threadId, initSequence);
+            AddNewProducer(thisServerServivceThread);
+         }
+         catch (UnknownHostException e)
+         {
+            e.printStackTrace();
+         }
+         catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+      }
+      
+      for(int i = 0; i < destinationNodeCount; i++)
+      {
+         String connectionInfo[] = destinationNodesArray[i].split(":");
+         int connectionPort = Integer.parseInt(connectionInfo[1]);
+         System.out.println(connectionInfo[0] + " " + Integer.toString(connectionPort));
+         try 
+         {
+            Socket thisSocket = new Socket(connectionInfo[0], connectionPort);
+            SubscriberServiceThread thisServerServivceThread = new SubscriberServiceThread(this, thisSocket, threadId, null, null);
+            AddNewConsumer(thisServerServivceThread);
+         }
+         catch (UnknownHostException ex)
+         {
+            System.out.println("Server not found: " + ex.getMessage());
+         }
+         catch (IOException ex)
+         {
+            System.out.println("I/O error: " + ex.getMessage());
+         }
+      }
+      
+      //Start all of the threads. We start the destination nodes first so that no data from the sources is acceptd
+      for(int i = 0; i < producerList.size(); i++)
+      {
+         producerList.get(i).start();
+      }
+      for(int i = 0; i < consumerList.size(); i++)
+      {
+         consumerList.get(i).start();
+      }
+      
+   }
+   
+   public static void main (String[] args)
+   {
+      new AutoDeletePlugin();
+   }
+
+
+}
+
---
+++ distributed/plugins/AutoPublisherServiceThread.java
@@ -0,0 +1,176 @@
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import java.net.Socket;
+
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.TimerTask;
+
+import VDMS.protobufs.QueryMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+class AutoPublisherServiceThread extends PublisherServiceThread
+{ 
+    BlockingQueue<VdmsTransaction> outboundAutoQueue;
+    int autoQueryDelay;
+    int autoQueryPeriod;
+    
+    public AutoPublisherServiceThread()
+    { 
+        super();
+        autoQueryDelay = 1000;
+        autoQueryPeriod = 2000;
+    } 
+    
+    AutoPublisherServiceThread(Plugin nManager, Socket s, int nThreadId, VdmsTransaction nInitSequence) 
+    {
+        super(nManager, s, nThreadId, nInitSequence);
+        outboundAutoQueue = new ArrayBlockingQueue<VdmsTransaction>(128);
+        autoQueryDelay = 1000;
+        autoQueryPeriod = 2000;
+    } 
+    
+    
+    public void Publish(VdmsTransaction newMessage)
+    {
+        responseQueue.add(newMessage);
+    }
+
+
+    
+    public void run() 
+    { 
+        DataOutputStream out = null;
+        VdmsTransaction returnedMessage;
+        Boolean threadInitFlag = false;
+        VdmsTransaction newTransaction = null;
+        
+        try
+        { 
+            out = new DataOutputStream(serverSocket.getOutputStream());
+            
+            while(m_bRunThread) 
+            {
+                if(threadInitFlag == false)
+                {
+                    //only write the value if there is a valid init sequence
+                    if(initSequence != null)
+                    {
+                        out.write(initSequence.GetSize());
+                        out.write(initSequence.GetBuffer());
+
+
+                        AutoQueryTask timedQuery = new AutoQueryTask();
+                        Timer t = new Timer();
+                        t.schedule(timedQuery, 1000, 1000);
+                    }
+                    threadInitFlag = true;
+                }
+                                
+                newTransaction =  outboundAutoQueue.take();
+                manager.AddToConsumerQueue(newTransaction);
+                returnedMessage = responseQueue.take();
+                ++messageId;  
+                
+            }
+        }
+        catch(Exception e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        finally 
+        { 
+            try
+            { 
+                out.close(); 
+                serverSocket.close(); 
+                System.out.println("...Stopped"); 
+            }
+            catch(IOException ioe)
+            { 
+                ioe.printStackTrace(); 
+            } 
+        } 
+    } 
+
+
+
+   class AutoQueryTask extends TimerTask 
+   {
+      private VdmsTransaction autoQuery;
+
+      public AutoQueryTask()
+      {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
+        calendar.set(2011, Calendar.OCTOBER, 1);
+        long secondsSinceEpoch = calendar.getTimeInMillis() / 1000L;
+
+
+
+
+
+
+
+
+        The methods Calendar.getTimeInMillis() and Date.getTime() both return milliseconds since 1.1.1970.
+
+        For current time, you can use:
+        
+        long seconds = System.currentTimeMillis() / 1000l;
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        QueryMessage.queryMessage autoMessage = QueryMessage.queryMessage.newBuilder().setJson("{\'a\' : 5}").build();
+        int messageSizeInt = autoMessage.getSerializedSize();
+        byte messageSize[] = {(byte) (messageSizeInt & 0x000000ff), (byte) ((messageSizeInt & 0x0000ff00) >>> 8), (byte) ((messageSizeInt & 0x00ff0000) >>> 16),  (byte)((messageSizeInt & 0x00ff0000) >>> 24)};
+        autoQuery = new VdmsTransaction(messageSize, autoMessage.toByteArray());
+      }
+      public AutoQueryTask(VdmsTransaction nAutoQuery) 
+      {
+          autoQuery = nAutoQuery;
+      }
+      public void run() 
+      {
+         System.out.println("hello world");
+         //outboundAutoQueue.add(autoQuery);
+      }
+   }
+
+}
