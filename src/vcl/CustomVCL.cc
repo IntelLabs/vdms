734f7462fa8fccd1727ce98f86cf84a730bbd301
Dennis Matthews <dennis.matthews.87@gmail.com>
2021-08-12 22:22:04 -0700

    fixing tab formatting

--- src/vcl/CustomVCL.cc
+++ src/vcl/CustomVCL.cc
@@ -2,7 +2,6 @@
 
 void custom_vcl_function(VCL::Image& img, const Json::Value& ops)
 {
-cv::Mat tmp = img.get_cvmat(true);
-
-img.deep_copy_cv(tmp);
+    cv::Mat tmp = img.get_cvmat(true);
+    img.deep_copy_cv(tmp);
 }
