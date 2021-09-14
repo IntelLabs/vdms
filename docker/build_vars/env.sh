03bb430ae2b0e5f60d60efe24ee99a0a4eddd884
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-13 18:06:31 -0700

    update env

--- docker/build_vars/env.sh
+++ docker/build_vars/env.sh
@@ -1,1 +1,1 @@
-MAVEN_OPTS="-Dhttp.proxyHost=proxy-chain.intel.com -Dhttp.proxyPort=911 -Dhttps.proxyHost=proxy-chain.intel.com -Dhttps.proxyPort=912"
+MAVEN_OPTS='-Dhttps.proxyHost="proxy-chain.intel.com" -Dhttps.proxyPort=912 -Dhttps.nonProxyHosts="localhost|127.0.0.1"'
