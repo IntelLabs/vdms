716360c6e1890d14f48955a9b4cddb7ba7183f15
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-13 23:55:35 -0700

    add different docker for test

--- docker/build_vars/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Read in the file of environment settings
-./build/env.sh
-# Then run the CMD
-# exec "$@"
-echo "$MAVEN_OPTS" > /tmp/testfile
-MAVEN_OPTS=$MAVEN_OPTS
--- docker/build_vars/env.sh
+++
@@ -1,1 +0,0 @@
-MAVEN_OPTS='-Dhttps.proxyHost="proxy-chain.intel.com" -Dhttps.proxyPort=912 -Dhttps.nonProxyHosts="localhost|127.0.0.1"'
