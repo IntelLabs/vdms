a610832e8d126b9a6481381e3dcb535f3b98802e
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-16 08:24:09 -0700

    update ci to fix artifact

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,7 @@ mkdir videos_tests
 echo 'Running C++ tests...'
 ./../build/tests/unit_test \
     --gtest_filter=-Descriptors_Add.add_flatl2_100d_2add:Descriptors_Store.add_ivfflatl2_100d_2add_file:ImageTest.CreateNameTDB:ImageTest.NoMetadata:PMGDQueryHandler.queryUpdateConstraintTest:PMGDQueryHandler.queryEdgeTestList:PMGDQueryHandler.queryEdgeTestSortList:PMGDQueryHandler.queryNodeEdgeTestList:QueryHandler.AddAndFind:VideoTest.CreateUnique
-gcovr --xml-pretty --exclude-unreachable-branches --print-summary -o c_coverage_report.xml --root /vdms
+gcovr --xml-pretty -p --exclude-unreachable-branches --print-summary -o c_coverage_report.xml --root /vdms
 
 # echo 'Running Python tests...'
 # cd python
