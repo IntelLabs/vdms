b0576959a82e974ef05f61710665542c449eed86
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-16 00:42:56 -0700

    add c++ coverage tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,11 @@ mkdir dbs  # necessary for Descriptors
 mkdir temp # necessary for Videos
 mkdir videos_tests
 
-./unit_test
-echo 'Running python tests...'
-cd python
-sh run_python_tests.sh
+echo 'Running C++ tests...'
+./../build/tests/unit_test \
+    --gtest_filter=-Descriptors_Add.add_flatl2_100d_2add:Descriptors_Store.add_ivfflatl2_100d_2add_file:ImageTest.CreateNameTDB:ImageTest.NoMetadata:PMGDQueryHandler.queryUpdateConstraintTest:PMGDQueryHandler.queryEdgeTestList:PMGDQueryHandler.queryEdgeTestSortList:PMGDQueryHandler.queryNodeEdgeTestList:QueryHandler.AddAndFind:VideoTest.CreateUnique
+gcovr --xml-pretty --exclude-unreachable-branches --print-summary -o c_coverage_report.xml --root /vdms
+
+# echo 'Running Python tests...'
+# cd python
+# sh run_python_tests.sh
