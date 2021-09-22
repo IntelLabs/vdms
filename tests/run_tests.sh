19efa6cd0ffa9342c0ed0bfcf15aadd4f073715f
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-22 01:11:04 -0700

    C++ coverage for files in src/ and include/

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,7 @@ mkdir dbs  # necessary for Descriptors
 mkdir temp # necessary for Videos
 mkdir videos_tests
 
+# Gets coverage for files in ../src and ../include
 echo 'Running C++ tests...'
 ./../build/tests/unit_test \
     --gtest_filter=-Descriptors_Add.add_flatl2_100d_2add:Descriptors_Store.add_ivfflatl2_100d_2add_file:ImageTest.CreateNameTDB:ImageTest.NoMetadata:PMGDQueryHandler.queryUpdateConstraintTest:PMGDQueryHandler.queryEdgeTestList:PMGDQueryHandler.queryEdgeTestSortList:PMGDQueryHandler.queryNodeEdgeTestList:QueryHandler.AddAndFind:VideoTest.CreateUnique
@@ -11,6 +12,8 @@ gcovr -k --root /vdms \
     -e /vdms/tests \
     -e /vdms/src/pmgd \
     -e /vdms/build \
+    -e /vdms/client \
+    -e /vdms/utils \
     --exclude-unreachable-branches \
     --print-summary --xml-pretty -o c_coverage_report.xml
 
