428bfef14009a5b2cf84fcbc8792e212bcc8d86c
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-21 19:49:50 -0700

    Update coverage of all C++ source code

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,13 @@ mkdir videos_tests
 echo 'Running C++ tests...'
 ./../build/tests/unit_test \
     --gtest_filter=-Descriptors_Add.add_flatl2_100d_2add:Descriptors_Store.add_ivfflatl2_100d_2add_file:ImageTest.CreateNameTDB:ImageTest.NoMetadata:PMGDQueryHandler.queryUpdateConstraintTest:PMGDQueryHandler.queryEdgeTestList:PMGDQueryHandler.queryEdgeTestSortList:PMGDQueryHandler.queryNodeEdgeTestList:QueryHandler.AddAndFind:VideoTest.CreateUnique
-gcovr --xml-pretty -p --exclude-unreachable-branches --print-summary -o c_coverage_report.xml --root /vdms
+gcovr -k --root /vdms \
+    -e /vdms/tests \
+    -e /vdms/src/pmgd \
+    -e /vdms/build \
+    --exclude-unreachable-branches \
+    --print-summary --xml-pretty -o c_coverage_report.xml
+
 
 # echo 'Running Python tests...'
 # cd python
