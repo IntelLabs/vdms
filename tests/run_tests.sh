e56da215a498bc462abb089e037983dd7e26e4c9
cwlacewe <chaunte.w.lacewell@intel.com>
2021-09-22 10:23:50 -0700

    show cpp coverage as table to provide info to developer

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,15 +8,22 @@ mkdir videos_tests
 echo 'Running C++ tests...'
 ./../build/tests/unit_test \
     --gtest_filter=-Descriptors_Add.add_flatl2_100d_2add:Descriptors_Store.add_ivfflatl2_100d_2add_file:ImageTest.CreateNameTDB:ImageTest.NoMetadata:PMGDQueryHandler.queryUpdateConstraintTest:PMGDQueryHandler.queryEdgeTestList:PMGDQueryHandler.queryEdgeTestSortList:PMGDQueryHandler.queryNodeEdgeTestList:QueryHandler.AddAndFind:VideoTest.CreateUnique
-gcovr -k --root /vdms \
-    -e /vdms/tests \
-    -e /vdms/src/pmgd \
-    -e /vdms/build \
-    -e /vdms/client \
-    -e /vdms/utils \
-    --exclude-unreachable-branches \
-    --print-summary --xml-pretty -o c_coverage_report.xml
+# gcovr -k --root /vdms \
+#     -e /vdms/tests \
+#     -e /vdms/src/pmgd \
+#     -e /vdms/build \
+#     -e /vdms/client \
+#     -e /vdms/utils \
+#     --exclude-unreachable-branches \
+#     --xml-pretty -o c_coverage_report.xml
+    #  --print-summary 
 
+gcovr -k --root /vdms \
+    -f /vdms/src \
+    -f /vdms/include \
+    -e /vdms/src/pmgd \
+    --exclude-unreachable-branches \
+    -o c_coverage_report.xml
 
 # echo 'Running Python tests...'
 # cd python
