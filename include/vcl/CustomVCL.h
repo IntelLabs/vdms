e662681896228cade17e402a22b520cc3b35020e
Dennis Matthews <dennis.matthews.87@gmail.com>
2021-11-03 12:37:26 -0700

    checking in code to switch to having extensions stored in a subdirectory

--- include/vcl/CustomVCL.h
+++ include/vcl/CustomVCL.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+
 #include <jsoncpp/json/value.h>
+
+#include <opencv2/opencv.hpp>
+
 #include "Image.h"
 
-void custom_vcl_function(VCL::Image& img, const Json::Value& ops);
+#define SHARED_IMAGE_BUFFER_SIZE 134217728
+
+// structure for message queue
+//first byte of message must be non negative long
+typedef struct mesg_buffer {
+    long message_type;
+    unsigned int data_rows;
+    unsigned int data_cols;
+    unsigned int data_type;
+    unsigned int data_image_size;
+    unsigned int data_json_size;
+} message;
+
+
+//void custom_vcl_function(VCL::Image& img, const Json::Value& ops);
--- src/ImageCommand.cc
+++ src/ImageCommand.cc
@@ -75,7 +75,8 @@ void ImageCommand::enqueue_operations(VCL::Image& img, const Json::Value& ops)
             VCL::Image* tmp_image = new VCL::Image(img , true);
             try
             {
-                custom_vcl_function(img, op);
+                //custom_vcl_function(img, op);
+                //ddm add code to defer computation to local process
             }
             catch ( ... ) 
             {
