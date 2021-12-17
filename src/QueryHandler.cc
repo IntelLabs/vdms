6d5810cd8ecb466252d6b8dae165611901ae9698
Dennis D Matthews <dennis.d.matthews@intel.com>
2021-12-17 11:13:37 -0800

    adding FindFrames and FindDescriptors to nodes that populate autodelete queue on startup

--- src/QueryHandler.cc
+++ src/QueryHandler.cc
@@ -480,10 +480,9 @@ void QueryHandler::regualar_run_autodelete()
     delete json_string;
 }
 
-
 void QueryHandler::build_autodelete_queue()
 {
-    std::string* json_string = new std::string("[{\"FindImage\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}, {\"FindVideo\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}], {\"FindEntity\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}");
+    std::string* json_string = new std::string("[{\"FindImage\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}, {\"FindVideo\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}], {\"FindFrames\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}], {\"FindDescriptor\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}], {\"FindEntity\": {\"results\": {\"list\": [\"_expiration\"]}, \"constraints\": {\"_expiration\": [\">\", 0]}}}");
     protobufs::queryMessage response;
     protobufs::queryMessage query;
     query.set_json(json_string->c_str());
