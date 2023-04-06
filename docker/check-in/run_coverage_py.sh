2812703278d700ec3db20cfd0cf237fd3033070a
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2023-04-06 11:49:04 -0700

    Remove internal files

--- docker/check-in/run_coverage_py.sh
+++
@@ -1,7 +0,0 @@
-cd /vdms/tests/python
-
-./run_python_tests.sh
-python -m coverage report -m 2>&1 | tee /vdms/tests/coverage_report/py_coverage_report.txt
-python -m coverage xml -o /vdms/tests/coverage_report/py_coverage_report.xml
-
-echo "DONE"
