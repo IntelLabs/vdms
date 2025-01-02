47ad765dc6622d0f48f896cfd2f0adb996b24fb3
100rish <sourish.chatterjee@intel.com>
2025-01-02 11:39:36 -0800

    latest
    k8sfixes

---
+++ custom_bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /vdms/user_defined_operations/
+python3 -m pip install -r requirements.txt
+python3 -m pip install sk-video
+python3 -m pip install imutils
+python3 -m pip install zmq
