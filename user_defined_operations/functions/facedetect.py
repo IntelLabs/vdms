b81c9040b83f02b5da0edfe01c443d34ff884ab1
Rolando Quesada <rolando.quesada.jimenez@intel.com>
2025-03-18 16:33:39 -0700

    276 Add resources dir to the container

--- user_defined_operations/functions/facedetect.py
+++ user_defined_operations/functions/facedetect.py
@@ -2,11 +2,8 @@ import time
 import cv2
 import os
 
-# Get the real directory where this Python file is
-currentDir = os.path.realpath(os.path.dirname(__file__))
-
-haarcascade_frontalface_default_path = os.path.join(
-    currentDir, "../../resources/haarcascade_frontalface_default.xml"
+haarcascade_frontalface_default_path = (
+    "../../resources/haarcascade_frontalface_default.xml"
 )
 
 if not os.path.exists(haarcascade_frontalface_default_path):
